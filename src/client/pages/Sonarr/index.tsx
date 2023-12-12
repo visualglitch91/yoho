cdf28f048a5b3a7598027fe0f019d6cb483e6af2
visualglitch91 <visualglitch91@proton.me>
2023-12-12 19:27:26 -0300

    Browse shows

--- src/client/pages/Sonarr/index.tsx
+++ src/client/pages/Sonarr/index.tsx
@@ -1,23 +1,39 @@
+import { useState } from "react";
 import { orderBy } from "lodash";
 import { useQuery } from "@tanstack/react-query";
 import AppBar from "$common/Layout/AppBar";
 import { api } from "$common/utils";
 import { Box } from "@mui/material";
 import MediaItem from "$common/MediaItem";
+import SearchBar from "$common/SearchBar";
 
 export default function Sonarr() {
-  const $movies = useQuery({
-    queryKey: ["Sonarr", "Series"],
+  const [search, setSearch] = useState("");
+
+  const $series = useQuery({
+    queryKey: ["Sonarr", "Series", search],
     queryFn: () => {
       return Promise.all([
         api
-          .get("/sonarr/v3/series")
+          .get(
+            search
+              ? `/sonarr/v3/series/lookup?term=${search}`
+              : "/sonarr/v3/series"
+          )
           .then((res) =>
-            orderBy(res.data, ["sortTitle", "title"], ["asc", "asc"])
+            orderBy(res.data, ["sortTitle", "title"], ["asc", "asc"]).filter(
+              (it) =>
+                it.runtime !== 0 &&
+                it.images.find((it: any) => it.coverType === "poster")
+                  ?.remoteUrl &&
+                it.studio !== ""
+            )
           ),
         api.get("/sonarr/v3/queue/details?all=true").then((res) => res.data),
       ]).then(([series, queue]) => {
-        const seriesInQueue = queue.map((it: any) => it.seriesId);
+        const seriesInQueue = queue
+          .map((it: any) => it.seriesId)
+          .filter(Boolean);
 
         return series.map((it) => ({
           ...it,
@@ -33,6 +49,7 @@ export default function Sonarr() {
             coverType: string;
             remoteUrl: string;
           }[];
+          path: string;
           statistics: {
             episodeCount: number;
             episodeFileCount: number;
@@ -45,8 +62,19 @@ export default function Sonarr() {
   return (
     <>
       <AppBar title="Sonarr" />
+
+      <SearchBar
+        onSearch={(term) => {
+          if (search === term) {
+            $series.refetch();
+          } else {
+            setSearch(term);
+          }
+        }}
+      />
+
       <Box display="flex" flexWrap="wrap" gap={2} justifyContent="center">
-        {($movies.data || []).map((item) => {
+        {($series.data || []).map((item) => {
           const percent =
             item.statistics.episodeCount === 0
               ? 100
@@ -64,7 +92,9 @@ export default function Sonarr() {
               title={item.title}
               percent={percent}
               color={
-                item.downloading
+                !item.path
+                  ? "warning"
+                  : item.downloading
                   ? "primary"
                   : percent === 100
                   ? item.ended
